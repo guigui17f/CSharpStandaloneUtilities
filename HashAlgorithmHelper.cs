19711c24b06338826eccd28053b6a7d83b03fd90
guigui17f <guigui17f0@gmail.com>
2022-12-31 19:52:57 +0800

    add security utilities

---
+++ HashAlgorithmHelper.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GUIGUI17F
+{
+    public class HashAlgorithmHelper : IDisposable
+    {
+        public enum AlgorithmName
+        {
+            MD5,
+            HMACMD5,
+            SHA1,
+            HMACSHA1,
+            SHA256,
+            HMACSHA256,
+            SHA384,
+            HMACSHA384,
+            SHA512,
+            HMACSHA512
+        }
+        
+        public HashAlgorithm Algorithm => _hashAlgorithm;
+
+        private HashAlgorithm _hashAlgorithm;
+
+        /// <summary>
+        /// remember to call Dispose() when this instance is no longer needed
+        /// </summary>
+        public HashAlgorithmHelper(AlgorithmName algorithmName)
+        {
+            switch (algorithmName)
+            {
+                case AlgorithmName.MD5:
+                    _hashAlgorithm = MD5.Create();
+                    break;
+                case AlgorithmName.HMACMD5:
+                    _hashAlgorithm = HMAC.Create("HMACMD5");
+                    break;
+                case AlgorithmName.SHA1:
+                    _hashAlgorithm = SHA1.Create();
+                    break;
+                case AlgorithmName.HMACSHA1:
+                    _hashAlgorithm = HMAC.Create("HMACSHA1");
+                    break;
+                case AlgorithmName.SHA256:
+                    _hashAlgorithm = SHA256.Create();
+                    break;
+                case AlgorithmName.HMACSHA256:
+                    _hashAlgorithm = HMAC.Create("HMACSHA256");
+                    break;
+                case AlgorithmName.SHA384:
+                    _hashAlgorithm = SHA384.Create();
+                    break;
+                case AlgorithmName.HMACSHA384:
+                    _hashAlgorithm = HMAC.Create("HMACSHA384");
+                    break;
+                case AlgorithmName.SHA512:
+                    _hashAlgorithm = SHA512.Create();
+                    break;
+                case AlgorithmName.HMACSHA512:
+                    _hashAlgorithm = HMAC.Create("HMACSHA512");
+                    break;
+            }
+        }
+
+        public string GetHash(byte[] sourceData)
+        {
+            byte[] hash = GetHashData(sourceData);
+            return BitConverter.ToString(hash).Replace("-", string.Empty);
+        }
+
+        public byte[] GetHashData(byte[] sourceData)
+        {
+            return _hashAlgorithm.ComputeHash(sourceData);
+        }
+
+        public string GetHash(string sourceString, Encoding encoding)
+        {
+            byte[] hash = GetHashData(sourceString, encoding);
+            return BitConverter.ToString(hash).Replace("-", string.Empty);
+        }
+
+        public byte[] GetHashData(string sourceString, Encoding encoding)
+        {
+            return GetHashData(encoding.GetBytes(sourceString));
+        }
+
+        public string GetFileHash(string filePath)
+        {
+            byte[] hash = GetFileHashData(filePath);
+            if (hash == null)
+            {
+                return null;
+            }
+            return BitConverter.ToString(hash).Replace("-", string.Empty);
+        }
+
+        public byte[] GetFileHashData(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return null;
+            }
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                return _hashAlgorithm.ComputeHash(fs);
+            }
+        }
+
+        public void Dispose()
+        {
+            _hashAlgorithm.Dispose();
+        }
+    }
+}
---
+++ SymmetricAlgorithmHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GUIGUI17F
+{
+    public class SymmetricAlgorithmHelper : IDisposable
+    {
+        public enum AlgorithmName
+        {
+            DES,
+            AES,
+            Rijndael,
+            TripleDES,
+            RC2
+        }
+
+        public SymmetricAlgorithm Algorithm => _symmetricAlgorithm;
+
+        private SymmetricAlgorithm _symmetricAlgorithm;
+        private ICryptoTransform _encryptor;
+        private ICryptoTransform _decryptor;
+
+        /// <summary>
+        /// remember to call Dispose() when this instance is no longer needed
+        /// </summary>
+        public SymmetricAlgorithmHelper(AlgorithmName algorithmName, byte[] key, byte[] iv, bool autoInit, CipherMode mode = CipherMode.CBC, PaddingMode padding = PaddingMode.PKCS7)
+        {
+            switch (algorithmName)
+            {
+                case AlgorithmName.DES:
+                    _symmetricAlgorithm = DES.Create();
+                    break;
+                case AlgorithmName.AES:
+                    _symmetricAlgorithm = Aes.Create();
+                    break;
+                case AlgorithmName.Rijndael:
+                    _symmetricAlgorithm = Rijndael.Create();
+                    break;
+                case AlgorithmName.TripleDES:
+                    _symmetricAlgorithm = TripleDES.Create();
+                    break;
+                case AlgorithmName.RC2:
+                    _symmetricAlgorithm = RC2.Create();
+                    break;
+            }
+            _symmetricAlgorithm.Key = key;
+            _symmetricAlgorithm.IV = iv;
+            _symmetricAlgorithm.Mode = mode;
+            _symmetricAlgorithm.Padding = padding;
+            if (autoInit)
+            {
+                Initialize();
+            }
+        }
+
+        /// <summary>
+        /// call Initialize() after you setup custom parameters for Algorithm property
+        /// </summary>
+        public void Initialize()
+        {
+            _encryptor?.Dispose();
+            _decryptor?.Dispose();
+            _encryptor = _symmetricAlgorithm.CreateEncryptor();
+            _decryptor = _symmetricAlgorithm.CreateDecryptor();
+        }
+
+        public string Encrypt(string origin, Encoding encoding)
+        {
+            byte[] encryptedData = EncryptData(encoding.GetBytes(origin));
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public byte[] EncryptData(byte[] originData)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, _encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(originData, 0, originData.Length);
+                    cryptoStream.FlushFinalBlock();
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+        public string Decrypt(string encrypted, Encoding encoding)
+        {
+            byte[] originData = DecryptData(Convert.FromBase64String(encrypted));
+            return encoding.GetString(originData);
+        }
+
+        public byte[] DecryptData(byte[] encryptedData)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, _decryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(encryptedData, 0, encryptedData.Length);
+                    cryptoStream.FlushFinalBlock();
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+        public void Dispose()
+        {
+            _encryptor?.Dispose();
+            _decryptor?.Dispose();
+            _symmetricAlgorithm.Dispose();
+        }
+    }
+}
