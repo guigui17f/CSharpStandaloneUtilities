505a0b9516191a8a4819befb1e2b26e015d284c3
guigui17f <guigui17f0@gmail.com>
2022-12-31 19:55:08 +0800

    add a process watcher utility

---
+++ ProcessWatcher/ProcessWatchClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+namespace GUIGUI17F
+{
+    public class ProcessWatchClient
+    {
+        public Process OwnerProcess { get; private set; }
+        public string ExtraParameter { get; private set; }
+        public AutoResetEvent ResetEvent { get; private set; }
+
+        private string _pipeName = "GUIGUI17F.ProcessWatcher";
+        private string _key = "vOiyw4jkDGA4JNka8fBcA1aVnb20F+q3P4Fkz5a6Tn4=";
+        private string _iv = "hAZQBNV4T0tvq+5I28Mqhw==";
+        private string _ownerVerification = "Owner Verification";
+        private string _clientVerification = "Client Verification";
+
+        public bool Initialize()
+        {
+            bool success = false;
+            try
+            {
+                using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", _pipeName, PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Anonymous))
+                {
+                    pipeClient.Connect(800);
+                    if (pipeClient.IsConnected)
+                    {
+                        using (StreamReader sr = new StreamReader(pipeClient))
+                        {
+                            using (StreamWriter sw = new StreamWriter(pipeClient))
+                            {
+                                sw.AutoFlush = true;
+                                using (SymmetricAlgorithmHelper algorithmHelper = new SymmetricAlgorithmHelper(SymmetricAlgorithmHelper.AlgorithmName.AES, Convert.FromBase64String(_key), Convert.FromBase64String(_iv), true))
+                                {
+                                    if (algorithmHelper.Decrypt(sr.ReadLine(), Encoding.UTF8) == _ownerVerification)
+                                    {
+                                        sw.WriteLine(algorithmHelper.Encrypt(_clientVerification, Encoding.UTF8));
+                                        string pId = algorithmHelper.Decrypt(sr.ReadLine(), Encoding.UTF8);
+                                        ExtraParameter = algorithmHelper.Decrypt(sr.ReadLine(), Encoding.UTF8);
+                                        OwnerProcess = Process.GetProcessById(int.Parse(pId));
+                                        success = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    pipeClient.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                success = false;
+            }
+            return success;
+        }
+
+        public void RegisterProcessEvents(EventHandler exitedHandler, EventHandler disposedHandler, DataReceivedEventHandler outputDataReceivedHandler, DataReceivedEventHandler errorDataReceivedHandler)
+        {
+            OwnerProcess.EnableRaisingEvents = true;
+            if (exitedHandler != null)
+            {
+                OwnerProcess.Exited += exitedHandler;
+            }
+            if (disposedHandler != null)
+            {
+                OwnerProcess.Disposed += disposedHandler;
+            }
+            if (outputDataReceivedHandler != null)
+            {
+                OwnerProcess.OutputDataReceived += outputDataReceivedHandler;
+            }
+            if (errorDataReceivedHandler != null)
+            {
+                OwnerProcess.ErrorDataReceived += errorDataReceivedHandler;
+            }
+        }
+
+        public void SuspendThread()
+        {
+            if (ResetEvent == null)
+            {
+                ResetEvent = new AutoResetEvent(false);
+            }
+            else
+            {
+                ResetEvent.Reset();
+            }
+            ResetEvent.WaitOne();
+        }
+
+        public void ResumeThread()
+        {
+            if (ResetEvent != null)
+            {
+                ResetEvent.Set();
+            }
+        }
+    }
+}
---
+++ ProcessWatcher/ProcessWatchOwner.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace GUIGUI17F
+{
+    public class ProcessWatchOwner
+    {
+        public Process ClientProcess { get; private set; }
+        private string _pipeName = "GUIGUI17F.ProcessWatcher";
+        private string _key = "vOiyw4jkDGA4JNka8fBcA1aVnb20F+q3P4Fkz5a6Tn4=";
+        private string _iv = "hAZQBNV4T0tvq+5I28Mqhw==";
+        private string _ownerVerification = "Owner Verification";
+        private string _clientVerification = "Client Verification";
+        private string _clientPath;
+        private string _clientHash;
+
+        public bool Initialize(string clientPath, string clientHash)
+        {
+            if (!File.Exists(clientPath))
+            {
+                Console.WriteLine("client doesn't exist!");
+                return false;
+            }
+            using (HashAlgorithmHelper algorithmHelper = new HashAlgorithmHelper(HashAlgorithmHelper.AlgorithmName.SHA256))
+            {
+                if (algorithmHelper.GetFileHash(clientPath) != clientHash)
+                {
+                    Console.WriteLine("client hash verification failed!");
+                    return false;
+                }
+            }
+            _clientPath = clientPath;
+            _clientHash = clientHash;
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = Path.GetFullPath(_clientPath),
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true
+            };
+            ClientProcess = new Process
+            {
+                StartInfo = info
+            };
+            return true;
+        }
+
+        public bool StartWatcher(string extraParameter)
+        {
+            bool success = false;
+            try
+            {
+                ClientProcess.Start();
+                using (NamedPipeServerStream pipeServer = new NamedPipeServerStream(_pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message))
+                {
+                    pipeServer.WaitForConnection();
+                    using (StreamReader sr = new StreamReader(pipeServer))
+                    {
+                        using (StreamWriter sw = new StreamWriter(pipeServer))
+                        {
+                            sw.AutoFlush = true;
+                            using (SymmetricAlgorithmHelper algorithmHelper = new SymmetricAlgorithmHelper(SymmetricAlgorithmHelper.AlgorithmName.AES, Convert.FromBase64String(_key), Convert.FromBase64String(_iv), true))
+                            {
+                                sw.WriteLine(algorithmHelper.Encrypt(_ownerVerification, Encoding.UTF8));
+                                if (algorithmHelper.Decrypt(sr.ReadLine(), Encoding.UTF8) == _clientVerification)
+                                {
+                                    sw.WriteLine(algorithmHelper.Encrypt(Process.GetCurrentProcess().Id.ToString(), Encoding.UTF8));
+                                    sw.WriteLine(algorithmHelper.Encrypt(extraParameter, Encoding.UTF8));
+                                    success = true;
+                                }
+                            }
+                        }
+                    }
+                    pipeServer.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                success = false;
+            }
+            return success;
+        }
+
+        public void RegisterProcessEvents(EventHandler exitedHandler, EventHandler disposedHandler, DataReceivedEventHandler outputDataReceivedHandler, DataReceivedEventHandler errorDataReceivedHandler)
+        {
+            ClientProcess.EnableRaisingEvents = true;
+            if (exitedHandler != null)
+            {
+                ClientProcess.Exited += exitedHandler;
+            }
+            if (disposedHandler != null)
+            {
+                ClientProcess.Disposed += disposedHandler;
+            }
+            if (outputDataReceivedHandler != null)
+            {
+                ClientProcess.OutputDataReceived += outputDataReceivedHandler;
+            }
+            if (errorDataReceivedHandler != null)
+            {
+                ClientProcess.ErrorDataReceived += errorDataReceivedHandler;
+            }
+        }
+
+        public void UnRegisterProcessEvents(EventHandler exitedHandler, EventHandler disposedHandler, DataReceivedEventHandler outputDataReceivedHandler, DataReceivedEventHandler errorDataReceivedHandler)
+        {
+            if (exitedHandler != null)
+            {
+                ClientProcess.Exited -= exitedHandler;
+            }
+            if (disposedHandler != null)
+            {
+                ClientProcess.Disposed -= disposedHandler;
+            }
+            if (outputDataReceivedHandler != null)
+            {
+                ClientProcess.OutputDataReceived -= outputDataReceivedHandler;
+            }
+            if (errorDataReceivedHandler != null)
+            {
+                ClientProcess.ErrorDataReceived -= errorDataReceivedHandler;
+            }
+        }
+
+        /// <summary>
+        /// this will trigger ClientProcess.Exited and ClientProcess.Disposed events
+        /// </summary>
+        public bool ResetWatcher()
+        {
+            if (ClientProcess != null)
+            {
+                if (!ClientProcess.HasExited)
+                {
+                    ClientProcess.Close();
+                }
+                ClientProcess.Dispose();
+            }
+            return Initialize(_clientPath, _clientHash);
+        }
+    }
+}
